4ac28a12d65b04f1b2510a42bd83b7c8c19007e3
hishiyuu <hishiyuu.td@gmal.com>
2020-03-23 20:08:57 +0900

    first comit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <HelloWorld v-bind:msg="message" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// @ is an alias to /src(
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     HelloWorld
+  },
+  computed: {
+    message() {
+      const req = new XMLHttpRequest();
+      let message = "";
+      req.open("GET", "/foo", false);
+      req.onload = function() {
+        message = JSON.parse(req.responseText).msg;
+      };
+      req.send(null);
+      return message;
+    }
   }
-}
+};
 </script>
